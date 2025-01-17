9b5be489705857f56362d24df8b7fbfa923cb112
Suzerin <wwwww.stepen@gmail.com>
2025-01-17 20:14:29 +1100

    Revert "Добавил contacts в views.py"
    This reverts commit ba92d6eb5b860df80bb42d3da965c6013ec20291.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
 def index(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
 def contacts(request):
     return render(request, 'contacts.html')
+=======
+>>>>>>> parent of ba92d6e (Добавил contacts в views.py)
