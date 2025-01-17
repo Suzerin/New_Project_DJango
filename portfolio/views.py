9e0f6ecb406b7ba89b61724880da35e9ca54fd71
Suzerin <wwwww.stepen@gmail.com>
2025-01-17 20:16:36 +1100

    Подтверждение изменений

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 def index(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
-def contacts(request):
-    return render(request, 'contacts.html')
-=======
->>>>>>> parent of ba92d6e (Добавил contacts в views.py)
